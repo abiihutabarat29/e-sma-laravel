2b9166e1072db509943fa25caadf0bf3675d3c75
abiihutabarat29 <abiihutabarat97@gmail.com>
2023-08-06 15:07:11 +0700

    add seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\UsersCabdis;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            [
+                'nik' => 202020202020202020,
+                'nama' => 'Administrator',
+                'nohp' => '082274884828',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('12345678'),
+                'role' => 1,
+                'status' => 1,
+            ],
+        ];
+        UsersCabdis::insert($data);
+    }
+}
